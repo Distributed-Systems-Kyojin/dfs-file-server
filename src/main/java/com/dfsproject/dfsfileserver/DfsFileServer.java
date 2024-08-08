5294ca73a2d094943ebdef7d208f91843e08df5b
akashTharuka <akash_tharuka@yahoo.com>
2024-08-08 20:25:40 +0530

    first commit basic file structure,

---
+++ src/main/java/com/dfsproject/dfsfileserver/DfsFileServer.java
@@ -0,0 +1,13 @@
+package com.dfsproject.dfsfileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsFileServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfsFileServer.class, args);
+    }
+
+}
---
+++ src/test/java/com/dfsproject/dfsfileserver/DfsFileServerTests.java
@@ -0,0 +1,13 @@
+package com.dfsproject.dfsfileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfsFileServerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
